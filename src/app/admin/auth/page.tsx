410d66907843b683f0cf285aa7567041451a7fa8
Axel Draws <ads22002@student.mdu.se>
2024-04-16 11:15:25 +0200

    Fixed some small things in auth

--- src/app/admin/auth/page.tsx
+++ src/app/admin/auth/page.tsx
@@ -4,7 +4,11 @@ import Image from "next/image";
 import Link from "next/link";
 import { FaDiscord, FaGithub } from "react-icons/fa";
 
-export default function Home() {
+
+export default function Auth({ searchParams: { token } }: { searchParams: { token: string }, params: {} }) {
+
+
+  // if token is in url 
   const signInAlternatives = {
     discord: {
       callback: () => {
@@ -20,11 +24,11 @@ export default function Home() {
 
   return (
     <div className="flex h-screen w-screen justify-center items-center ">
-        <Link href={"/"} className="absolute top-4 left-6 text-gray-500 dark:text-gray-400 flex gap-2 items-center">
+      <Link href={"/"} className="absolute top-4 left-6 text-gray-500 dark:text-gray-400 flex gap-2 items-center">
         <ChevronLeftIcon className="h-5 w-5 text-gray-500 dark:text-gray-400" />
         <p className="text-sm">Tillbaka</p>
-        </Link>
-            
+      </Link>
+
       <div className="px-32 py-12 flex flex-col justify-center items-center border-lg  bg-white dark:bg-black sm:shadow-md shadow-yellow-700/20 rounded-lg">
         <Image
           loading="lazy"
@@ -39,24 +43,25 @@ export default function Home() {
           <h1 className="font-medium flex items-center text-2xl ">Dalopanel</h1>
           <div className="flex flex-col gap-4 ">
 
-          <p className="text-center text-gray-500">
-            Logga in med
-          </p>
-          <hr className="w-1/2 mx-auto my-2 border-gray-300 dark:border-gray-700" />
-
-            <button
-              onClick={signInAlternatives.discord.callback}
-              className="bg-blue-600 text-white px-4 py-2 rounded-lg flex items-center gap-2"
-            >
-              <FaDiscord /> <span>Discord</span>
-            </button>
-            <button
-              onClick={signInAlternatives.github.callback}
-              className="bg-neutral-800 text-white px-4 py-2 rounded-lg flex items-center gap-2 justify-center"
-            >
-              <FaGithub />
-              Github
-            </button>
+            <p className="text-center text-gray-500">
+              {token ? "Skapa konto med" : "Logga in med"}
+            </p>
+            <hr className="w-1/2 mx-auto my-2 border-gray-300 dark:border-gray-700" />
+            <div className="flex gap-3 sm:flex-row flex-col">
+              <button
+                onClick={signInAlternatives.discord.callback}
+                className="border-blue-600 border-2 text-blue-600 text-white px-4 py-2 rounded-lg flex items-center gap-2 justify-center"
+              >
+                <FaDiscord /> <span>Discord</span>
+              </button>
+              <button
+                onClick={signInAlternatives.github.callback}
+                className="border-neutral-800 border-2 text-neutral-800 text-white px-4 py-2 rounded-lg flex items-center gap-2 justify-center"
+              >
+                <FaGithub />
+                Github
+              </button>
+            </div>
           </div>
         </div>
       </div>
