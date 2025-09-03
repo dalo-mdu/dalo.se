3611d5984c18d84f216b2ba850791e79ab7bcd27
Johan Sandred <jsd21003@student.mdu.se>
2025-09-03 13:09:27 +0200

    Behandla The basic song som kod

--- src/app/(root)/songbook/page.tsx
+++ src/app/(root)/songbook/page.tsx
@@ -863,15 +863,13 @@ det går till öl och till brännvin.`
             name: "The basic song",
             melody: "Melodi: Mors lilla Olle",
             text:
-`10 LET oss nu fatta i våra glas
-
+`\`\`\`
+10 LET oss nu fatta i våra glas
 20 INPUT en klunk utav det som där has
-
 30 IF du fått nog THEN till 50 min vän
-
 40 ELSE GOTO-baka till 10 igen
-
-50 END`
+50 END
+\`\`\``
         }
     ]
 
